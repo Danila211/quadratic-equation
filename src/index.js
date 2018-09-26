012d2d597e0372c3ee641862554eb42e3ce6978d
danila211 <danila-211@mail.ru>
2018-09-26 23:28:34 +0300

    solveDiskriminant

--- src/index.js
+++ src/index.js
@@ -16,12 +16,12 @@ module.exports = function solveEquation(equation) {
 
     let discrim = Math.sqrt(b * b - 4 * a * c);
 
-    if (discrim > 0){
+    if (discrim > 0) {
       x1 = Math.round((-b - discrim) / (a * 2)); 
       x2 = Math.round((-b + discrim) / (a * 2));
       copyArr.push(x1);
       copyArr.push(x2);
-    }else if(discrim == 0){
+    }else if(discrim == 0) {
       x1 = Math.round(-(b/(2*a)));
       copyArr.push(x1);
     }
